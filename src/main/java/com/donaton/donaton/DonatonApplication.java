965aeb4c1bbbcd244ed593995b39a1e5fc89d0cd
The_MisterG <robi.contreras@duocuc.cl>
2026-05-07 00:18:31 +0200

    init: estructura base proyecto donaton

---
+++ src/main/java/com/donaton/donaton/DonatonApplication.java
@@ -0,0 +1,13 @@
+package com.donaton.donaton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonatonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonatonApplication.class, args);
+	}
+
+}
